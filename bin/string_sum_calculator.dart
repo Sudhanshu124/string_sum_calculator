5f118aa247c2ec105068bdefe535642bd6988dcb
Sudhanshu Tiwari <sudhanshutiwari@Sudhanshus-MacBook-Air.local>
2025-08-29 12:06:20 +0530

    Project setup

---
+++ bin/string_sum_calculator.dart
@@ -0,0 +1,5 @@
+import 'package:string_sum_calculator/string_sum_calculator.dart' as string_sum_calculator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${string_sum_calculator.calculate()}!');
+}
---
+++ test/string_sum_calculator_test.dart
@@ -0,0 +1,8 @@
+import 'package:string_sum_calculator/string_sum_calculator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
