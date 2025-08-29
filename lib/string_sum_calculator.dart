7c0bcd876653a0391e8cc535272bf5f7c54c80be
Sudhanshu Tiwari <sudhanshutiwari@Sudhanshus-MacBook-Air.local>
2025-08-29 14:52:21 +0530

    Fix for if string start with //

--- lib/string_sum_calculator.dart
+++ lib/string_sum_calculator.dart
@@ -3,13 +3,20 @@ class StringSumCalculator {
     if (s.isEmpty) {
       return 0;
     }
-    List<int> multipleNumber = _parseNumber(s);
+    String separator = ',';
+    String numbersToProcess = s;
+    if (s.startsWith('//')) {
+      int indexOfSeperatorEnd = s.indexOf('\n');
+      separator = s.substring(2, indexOfSeperatorEnd);
+      numbersToProcess = s.substring(indexOfSeperatorEnd + 1);
+    }
+    List<int> multipleNumber = _parseNumber(numbersToProcess, separator);
     return (multipleNumber.reduce((a, b) => a + b));
   }
 
-/* Function to format string according to our requirement*/
-  List<int> _parseNumber(String numbers) {
-    String numberWithoutDelimiters = numbers.replaceAll('\n', ',');
+  /* Function to format string according to our requirement*/
+  List<int> _parseNumber(String numbers, String separator) {
+    String numberWithoutDelimiters = numbers.replaceAll('\n', separator);
     return numberWithoutDelimiters
         .split(',')
         .where((nums) => nums.isNotEmpty)
